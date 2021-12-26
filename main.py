bea5409c5c71f4174ce7c5eed05478fc21f295e7
Patrick.Heithoff@husco.com <Patrick.Heithoff@husco.com>
2021-12-26 10:54:30 -0600

    Updated team statistics from teamRankings.com

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import csv
 import re
 import os
 import sys
+import stat_extraction
 
 # Stats keys used in dictionary
-statsKeys = ["Seed", "Off Eff", "Avg Scoring Margin", "Eff FG%", "Off Reb%", "FT Rate", "Def Reb%", "Block%",
+statsKeys = ["Seed", "Off Eff", "Avg Scoring Margin", "Eff FG%", "Off Reb%", "FT%", "Def Reb%", "Block%",
              "Steal Per Poss", "Assist TO ratio", "Fouls per poss", "Def Eff", "Opp Eff FG %", "Win %", "KenPom Rank",
-             "Avg Height", "Avg Experience", "TO %", "Adj Efficiency Margin"]
+             "Avg Height", "Avg Experience", "TO %", "Adj Efficiency Margin",]
 headerFields = ["Team-Year"]+statsKeys[:]
 
 statsDict = {key: None for key in statsKeys}
 
 def writeTeamsDictToCSV(teamsDict):
-    with open("team_stats.csv", "w", newline='') as f:
+    with open("team_stats_updated.csv", "w", newline='') as f:
         w = csv.DictWriter(f, headerFields)
         w.writeheader()
         for k in teamsDict:
@@ -21,9 +22,9 @@ def writeTeamsDictToCSV(teamsDict):
 def readTeamDictFromCSV():
     teamsDictionary = dict()
     i = 0
-    with open("team_stats_current.csv", mode="r") as fhandle:
+    with open("team_stats.csv", mode="r") as fhandle:
         for line in fhandle:
-            words = line.split(",")
+            words = line.rstrip("\n").split(",")
             if i == 0:
                 headers = words.copy()
                 i += 1
@@ -38,11 +39,21 @@ def readTeamDictFromCSV():
     return teamsDictionary
 
 def writeResultsToCSV(result):
-    with open('results.csv', 'w', newline='') as out:
+    with open('results_updated.csv', 'w', newline='') as out:
         csv_out = csv.writer(out)
         csv_out.writerow(['Team1', 'Team2', 'Score'])
         for row in result:
             csv_out.writerow(row)
+            
+def readResultsFromCSV():
+    resultList = []
+    with open("results.csv", mode="r") as f:
+        for line in f:
+            words = line.rstrip("\n").split(",")
+            resultList.append(words)
+            
+    return resultList
+            
 
 if __name__ == "__main__":
     # Dictionary containing teams with their specific stats dictionary.
@@ -54,66 +65,66 @@ if __name__ == "__main__":
     from itertools import islice
 
 
-    directory = 'Bracket Results'
-    for filename in os.listdir(directory):
-        with open(directory + '/' + filename, 'r') as f:
-            year = f.name[16:20]
-            while True:
-                next_6_lines = list(islice(f, 6))
-                if not next_6_lines:
-                    break
-                #sort_lines(next_6_lines)
-                # Extract data from the 6 lines
-                line1 = re.sub('=', '= ', next_6_lines[0]).split()
-                line2 = re.sub('=', '= ', next_6_lines[1]).split()
-                line3 = re.sub('=', '= ', next_6_lines[2]).split()
-                line4 = re.sub('=', '= ', next_6_lines[3]).split()
-                line5 = re.sub('=', '= ', next_6_lines[4]).split()
-                line6 = re.sub('=', '= ', next_6_lines[5]).split()
+    # directory = 'Bracket Results'
+    # for filename in os.listdir(directory):
+    #     with open(directory + '/' + filename, 'r') as f:
+    #         year = f.name[16:20]
+    #         while True:
+    #             next_6_lines = list(islice(f, 6))
+    #             if not next_6_lines:
+    #                 break
+    #             #sort_lines(next_6_lines)
+    #             # Extract data from the 6 lines
+    #             line1 = re.sub('=', '= ', next_6_lines[0]).split()
+    #             line2 = re.sub('=', '= ', next_6_lines[1]).split()
+    #             line3 = re.sub('=', '= ', next_6_lines[2]).split()
+    #             line4 = re.sub('=', '= ', next_6_lines[3]).split()
+    #             line5 = re.sub('=', '= ', next_6_lines[4]).split()
+    #             line6 = re.sub('=', '= ', next_6_lines[5]).split()
 
-                team1 = line2[2:]
-                team1 = ' '.join(team1)
-                temp = re.search("XX(.+?)]", team1)
-                if temp is not None:
-                    team1 = temp.group(1)
-                team1 = re.sub("'''", '', team1)
-                team1_year = team1 + '-' + year
+    #             team1 = line2[2:]
+    #             team1 = ' '.join(team1)
+    #             temp = re.search("XX(.+?)]", team1)
+    #             if temp is not None:
+    #                 team1 = temp.group(1)
+    #             team1 = re.sub("'''", '', team1)
+    #             team1_year = team1 + '-' + year
 
-                team1_score = re.sub("'", '', line3[2])
-                team1_score = re.sub("\*", '', team1_score)
-                team1_score = int(team1_score)
+    #             team1_score = re.sub("'", '', line3[2])
+    #             team1_score = re.sub("\*", '', team1_score)
+    #             team1_score = int(team1_score)
 
-                if team1_year not in teamsDict:
-                    seed1 = re.sub("'", '', line1[2])
-                    # print(seed1)
-                    # print(team1_year)
-                    teamsDict[team1_year] = dict(statsDict)
-                    teamsDict[team1_year]['Seed'] = int(seed1)
+    #             if team1_year not in teamsDict:
+    #                 seed1 = re.sub("'", '', line1[2])
+    #                 # print(seed1)
+    #                 # print(team1_year)
+    #                 teamsDict[team1_year] = dict(statsDict)
+    #                 teamsDict[team1_year]['Seed'] = int(seed1)
 
-                team2 = line5[2:]
-                team2 = ' '.join(team2)
-                temp = re.search("XX(.+?)]", team2)
-                if temp is not None:
-                    team2 = temp.group(1)
-                team2 = re.sub("'''", '', team2)
-                team2_year = team2 + '-' + year
-                team2_score = re.sub("'", '', line6[2])
-                team2_score = re.sub("\*", '', team2_score)
-                team2_score = int(team2_score)
-                if team2_year not in teamsDict:
-                    seed2 = re.sub("'", '', line4[2])
-                    # print(seed2)
-                    # print(team2_year)
-                    teamsDict[team2_year] = dict(statsDict)
-                    teamsDict[team2_year]['Seed'] = int(seed2)
+    #             team2 = line5[2:]
+    #             team2 = ' '.join(team2)
+    #             temp = re.search("XX(.+?)]", team2)
+    #             if temp is not None:
+    #                 team2 = temp.group(1)
+    #             team2 = re.sub("'''", '', team2)
+    #             team2_year = team2 + '-' + year
+    #             team2_score = re.sub("'", '', line6[2])
+    #             team2_score = re.sub("\*", '', team2_score)
+    #             team2_score = int(team2_score)
+    #             if team2_year not in teamsDict:
+    #                 seed2 = re.sub("'", '', line4[2])
+    #                 # print(seed2)
+    #                 # print(team2_year)
+    #                 teamsDict[team2_year] = dict(statsDict)
+    #                 teamsDict[team2_year]['Seed'] = int(seed2)
 
 
-                resultTuple = (team1_year, team2_year, (team1_score - team2_score))
-                resultsList.append(resultTuple)
+    #             resultTuple = (team1_year, team2_year, (team1_score - team2_score))
+    #             resultsList.append(resultTuple)
 
-    # for result in resultsList:
-    #     print(result)
-    #
-    writeResultsToCSV(resultsList)
+    # writeResultsToCSV(resultsList)
 
-    readTeamDictFromCSV()
+    teamsDict = readTeamDictFromCSV()
+ 
+    updatedTeamsDict = stat_extraction.ExtractStatsFromTeamRankings(teamsDict)
+    writeTeamsDictToCSV(updatedTeamsDict)
---
+++ stat_extraction.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 24 08:56:26 2021
+
+@author: patrickh
+"""
+import urllib.request
+
+# Gets url string for the teamrankings website. If the specific stat does 
+# not exist then return "NaN"
+def getTeamRankingsUrl(stat, year):
+    #The following is the relationship between the stat in the dictionary
+    # to the url stat string.
+    urlStringDict = {
+                    "Off Eff": "offensive-efficiency",
+                    "Avg Scoring Margin": "average-scoring-margin",
+                    "Eff FG%": "effective-field-goal-pct",
+                    "Off Reb%": "offensive-rebounding-pct",
+                    "FT%": "free-throw-pct",
+                    "Def Reb%": "defensive-rebounding-pct",
+                    "Block%": "block-pct",
+                    "Steal Per Poss": "steals-perpossession",
+                    "Assist TO ratio": "assist--per--turnover-ratio",
+                    "Fouls per poss": "personal-fouls-per-possession",
+                    "Def Eff": "defensive-efficiency",
+                    "Opp Eff FG %": "opponent-effective-field-goal-pct",
+                    "Win %": "win-pct-all-games",
+                    "TO %": "turnovers-per-possession",                    
+        }
+    
+    # The url format for getting a specific stat is the following:
+    # https://www.teamrankings.com/ncaa-basketball/stat/{NAME_OF_STAT}?date={YEAR}-{MONTH}-{DAY}
+    # March 10th is the month and day extracted because that is before the first
+    # march madness game
+    if stat in urlStringDict:
+        urlstring = "https://www.teamrankings.com/ncaa-basketball/stat/"+urlStringDict[stat]+"?data="+year+"-03-10"+"&date="+year+"-03-10"
+    else:
+        urlstring = "NaN"
+        
+    return urlstring
+
+def index_containing_substring(the_list, substring):
+    for i, s in enumerate(the_list):
+        if substring in s:
+              return i
+    return -1
+
+def convertNameforTeamRankings(searchTeamString):
+    resultString = searchTeamString
+
+    teamRankingsDict = { 
+                        "Ole Miss": "Mississippi",
+                        "North Carolina State": "NC State",
+                        "Central Connecticut State": "Central Conn",
+                        "Southern California": "USC",
+                        "Florida Atlantic": "Fla Atlantic",
+                        "Pennsylvania": "U Penn",
+                        "Penn": "U Penn",
+                        "UNC-Wilmington": "NC-Wilmgton",
+                        "UNC Wilmington": "NC-Wilmgton", 
+                        "Michigan State": "Michigan St",
+                        "Western Kentucky": "W Kentucky",
+                        "San Diego State": "San Diego St",
+                        "Boston College": "Boston Col",
+                        "McNeese State": "McNeese St",
+                        "Boston University": "Boston U",
+                        "UC-Santa Barbara": "UCSB",
+                        "Illinois-Chicago": "IL-Chicago",
+                        "UW–Milwaukee": "WI-Milwkee",
+                        "Milwaukee": "WI-Milwkee",
+                        "Central Michigan": "Central Mich",
+                        "Colorado State": "Colorado St",
+                        "Arizona State": "Arizona St",
+                        "Utah St": "Utah State",
+                        "UNC-Asheville": "NC-Asheville",
+                        "Troy St": "Troy",
+                        "Sam Houston St": "Sam Hous St",
+                        "South Carolina St": "S Car State",
+                        "Saint Joseph's": "St Josephs",
+                        "East Tennessee State": "E Tenn St",
+                        "Morehead State": "Morehead St",
+                        "Alabama State": "Alabama St",
+                        "Cleveland State": "Cleveland St",
+                        "West Virginia": "W Virginia",
+                        "North Dakota State": "N Dakota St",
+                        "Robert Morris": "Rob Morris",
+                        "Texas A&M": "Texas A&amp;M",
+                        "Northern Iowa": "N Iowa",
+                        "Cal State Northridge": "Cal St Nrdge",
+                        "Oklahoma State": "Oklahoma St",
+                        "Florida St.": "Florida St",
+                        "Portland St.": "Portland St",
+                        "North Carolina": "N Carolina",
+                        "Stephen F. Austin": "Ste F Austin",
+                        "Morgan State": "Morgan St",
+                        "UTSA": "TX-San Ant",
+                        "Arkansas-Little Rock": "AR Lit Rock",
+                        "Little Rock": "AR Lit Rock",
+                        "Indiana State": "Indiana St",
+                        "George Mason": "Geo Mason",
+                        "Northern Colorado": "N Colorado",
+                        "Mississippi State": "Miss State",
+                        "Mississippi St": "Miss State",
+                        "Saint Peter's": "St Peters",
+                        "Florida State": "Florida St",
+                        "UNC Asheville": "NC-Asheville",
+                        "Kansas State": "Kansas St",
+                        "UC Santa Barbara": "UCSB",
+                        "Mississippi Valley State": "Miss Val St",
+                        "South Florida": "S Florida",
+                        "Wichita State": "Wichita St",
+                        "South Dakota State": "S Dakota St",
+                        "Norfolk State": "Norfolk St",
+                        "Long Beach State": "Lg Beach St",
+                        "Southern Miss": "S Mississippi",
+                        "St. Bonaventure": "St Bonavent",
+                        "Loyola (MD)": "Loyola-MD",
+                        "Loyola–Chicago": "Loyola-Chi",
+                        "Ohio St": "Ohio State",
+                        "Middle Tennessee": "Middle Tenn",
+                        "Saint Mary's": "St Marys",
+                        "James Madison": "James Mad",
+                        "New Mexico State": "N Mex State",
+                        "North Carolina A&T": "NC A&amp;T",
+                        "Long Island": "LIU",
+                        "Northwestern State": "NW State",
+                        "Florida Gulf Coast": "Fla Gulf Cst",
+                        "Mount St. Mary's": "Mt St Marys",
+                        "Texas Southern": "TX Southern",
+                        "Western Michigan": "W Michigan",
+                        "Eastern Kentucky": "E Kentucky",
+                        "Coastal Carolina": "Coastal Car",
+                        "George Washington": "Geo Wshgtn",
+                        "UConn": "Connecticut",
+                        "North Florida": "N Florida",
+                        "Louisiana–Lafayette": "LA Lafayette",
+                        "Massachusetts": "U Mass",
+                        "Georgia State": "Georgia St",
+                        "Northeastern": "Northeastrn",
+                        "Virginia Commonwealth": "VCU",
+                        "California-Irvine": "UC Irvine",
+                        "Eastern Washington": "E Washingtn",
+                        "Southern Methodist": "S Methodist",
+                        "SMU": "S Methodist",
+                        "Fairleigh Dickinson": "F Dickinson",
+                        "Green Bay": "WI-Grn Bay",
+                        "Oregon State": "Oregon St",
+                        "Cal State Bakersfield": "CS Bakersfld",
+                        "Fresno State": "Fresno St",
+                        "Virginia Tech": "VA Tech",
+                        "South Carolina": "S Carolina",
+                        "Jacksonville State": "Jksnville St",
+                        "Northern Kentucky": "N Kentucky",
+                        "UMBC": "Maryland BC",
+                        "UNC Greensboro": "NC-Grnsboro",                  
+                        "Cal State Fullerton": "CS Fullerton",
+                        "College of Charleston": "Col Charlestn",
+                        "TCU": "TX Christian",
+                        "UCF": "Central FL",
+                        "Gardner–Webb": "Gard-Webb",
+                        "Abilene Christian": "Abl Christian",
+                        "Grand Canyon": "Grd Canyon",
+                        "Georgia Tech": "GA Tech",
+        }
+    
+    if searchTeamString in teamRankingsDict:
+        resultString = teamRankingsDict[searchTeamString]
+    
+    return resultString
+
+# The purpose of this function is to parse through all the teams given as 
+# defined in the dictionary given and add to that dictionary from stats
+# located in teamrankings.com.
+def ExtractStatsFromTeamRankings(teamsDict):
+    statsKeys = list(list(teamsDict.values())[0].keys())
+    print(statsKeys)
+        
+    urlstring = ""
+    prevurlstring = ""
+    i = 0
+    
+    notFoundList = []
+    # Loop through all the stats in the teamsDict per team
+    for stat in statsKeys:
+        print(stat)
+        # Loop through all the teams in the teamsDict
+        for team in teamsDict:
+            print(team)
+            
+            # Check if team and stat is N/A or not, indicating that stat
+            # has not been looked up yet
+            if(teamsDict[team][stat] == "N/A"):
+                 # Get the last 4 characters to get year
+                year = team[-4:]
+                teamName = team[:-5]
+
+                #
+                prevurlstring = urlstring
+                urlstring = getTeamRankingsUrl(stat, year)
+                
+                # If this is a new url then open the url and get the list of strings
+                # for the data.
+                if urlstring != "NaN" and prevurlstring != urlstring:
+                    i = i+1
+                    fp = urllib.request.urlopen(urlstring)
+                    mybytes = fp.read()
+
+                    mystr = mybytes.decode("utf8")
+                    lineList = mystr.split('\n')
+                    #find the index of where the table starts denoted by the
+                    # following string: '\t<table class="tr-table datatable scrollable">'
+                    startOfTableIndex = lineList.index('\t<table class="tr-table datatable scrollable">')
+                    dataList = lineList[startOfTableIndex:]
+                    endOfTableIndex = dataList.index('</table>')
+                    dataList = dataList[:endOfTableIndex]
+                    #print(dataList)
+                    fp.close()
+                
+                if urlstring != "NaN":
+                    searchTeamString = teamName
+
+                    teamIndex = index_containing_substring(dataList, searchTeamString)
+                    
+                    if teamIndex == -1:
+                        notFoundList.append(searchTeamString)
+                        print("NOT FOUND:")
+                        print(searchTeamString)
+                    else:
+                        string = dataList[teamIndex+1]
+                        indexStart = string.find('sort="')+len('sort="')
+                        indexEnd = string.find('">')
+                        teamsDict[team][stat] = string[indexStart:indexEnd]
+        
+                    
+    textfile = open("notfound.txt", "w")
+    for element in notFoundList:
+        textfile.write(element + "\n")
+    textfile.close()
+                
+                    
+    return teamsDict    
+                
+    #urlstring = getTeamRankingsUrl()
